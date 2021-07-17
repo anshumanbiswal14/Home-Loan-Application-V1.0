e46d643a8c5a0a9dabaf90c07d6c5162fee49e9a
Anshuman Biswal <anshumanbiswal14@gmail.com>
2021-07-17 09:58:47 +0530

    First Edit

--- Home Loan App Sprint-2 Git/src/main/java/com/cg/homeloan/HomeLoanAppApplication.java
+++ Home Loan App Sprint-2 Git/src/main/java/com/cg/homeloan/HomeLoanAppApplication.java
@@ -5,18 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Team 3 "HomeLoan Application"
- * 
- * @author Aaradhya_Jain
- * @author Saish_karne
- * @author Pranchul_Jain
- * @author Rohit_Upadhyay
- * @author Noorul_Hasan
- * @author Omkar_Jagdale
- *
- */
-
 @SpringBootApplication
 public class HomeLoanAppApplication {
 	private static final Logger LOG = LoggerFactory.getLogger(HomeLoanAppApplication.class);
